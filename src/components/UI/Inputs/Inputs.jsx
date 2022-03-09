37ed2f21bcca72016196c69d812f4e728ec00f9a
kevinA2208 <usamo489@gmail.com>
2022-03-09 17:27:42 -0500

    Mail Contact Form

--- src/components/UI/Inputs/Inputs.jsx
+++ src/components/UI/Inputs/Inputs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Inputs = ({tipo, texto, id}) => {
+export const Inputs = ({name, tipo, texto, id}) => {
   return (
     <div>
-        <input type={tipo} placeholder={texto} id={id}></input>
+        <input name={name} type={tipo} placeholder={texto} id={id}></input>
     </div>
   )
 }
--- src/components/layouts/ContactMain/ContactMain.jsx
+++ src/components/layouts/ContactMain/ContactMain.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import { Inputs } from '../../UI/Inputs/Inputs'
-
+import emailjs from '@emailjs/browser'
 
 export const ContactMain = () => {
+
+  const sendEmail = (event) =>{
+    event.preventDefault()
+
+    emailjs.sendForm('service_sc0puiu' , 'template_8avmw6m' , event.target, 'oQ15VvVhPhKYEcYzP')
+
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+  }
+
+
   return (
     <div id="contactDiv">
-        <form id="contactForm">
+        <form id="contactForm" onSubmit={sendEmail}>
+            <h1 id='titleContact'>Contact us</h1>
             <h3 id="nameContact">Nombre:</h3>
-            <Inputs tipo="text"  texto="Ingrese su nombre" id="nameForm"/>
+            <Inputs name="user_name" tipo="text"  texto="Ingrese su nombre" id="nameForm"/>
+
             <h3 id="emailContact">Correo:</h3>
-            <Inputs tipo="mail"  texto="Ingrese su email" id="mailForm"/>
+            <Inputs name="user_email" tipo="mail"  texto="Ingrese su email" id="mailForm"/>
+
             <h3 id="msgContact">Mensaje:</h3>
-            <textarea id="msgForm" placeholder="Ingrese el texto"></textarea>
+            <textarea name="user_message" id="msgForm" placeholder="Ingrese el texto"></textarea>
+
             <Inputs tipo="submit" id="submitBtn"/>
         </form>
     </div>
